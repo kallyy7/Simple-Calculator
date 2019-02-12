91cd370fac85d6f0c76d59adc572c7c6d4ba56ab
Kalina <milanova1203@gmail.com>
2019-02-12 19:49:21 +0200

    edit

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -2,83 +2,142 @@
 {
     using System.Windows;
     using System.Windows.Controls;
+    using System.Windows.Input;
 
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private bool newNumbers = true;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void KeyDown_(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {             
+                case Key.NumPad0:
+                    ResultWindow.Text += "0";
+                    break;
+                case Key.NumPad1:
+                    ResultWindow.Text += "1";
+                    break;
+                case Key.NumPad2:
+                    ResultWindow.Text += "2";
+                    break;
+                case Key.NumPad3:
+                    ResultWindow.Text += "3";
+                    break;
+                case Key.NumPad4:
+                    ResultWindow.Text += "4";
+                    break;
+                case Key.NumPad5:
+                    ResultWindow.Text += "5";
+                    break;
+                case Key.NumPad6:
+                    ResultWindow.Text += "6";
+                    break;
+                case Key.NumPad7:
+                    ResultWindow.Text += "7";
+                    break;
+                case Key.NumPad8:
+                    ResultWindow.Text += "8";
+                    break;
+                case Key.NumPad9:
+                    ResultWindow.Text += "9";
+                    break;
+                case Key.Multiply:
+                    ResultWindow.Text += "*";
+                    break;
+                case Key.Divide:
+                    ResultWindow.Text += "/";
+                    break;
+                case Key.Subtract:
+                    ResultWindow.Text += "-";
+                    break;
+                case Key.OemPlus:
+                    ResultWindow.Text += "+";
+                    break;
+            }
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Button button = (Button)sender;
-            resultWindow.Text += button.Content.ToString();
+
+            if (newNumbers) ResultWindow.Text = "";
+            newNumbers = false;
+
+            ResultWindow.Text += button.Content.ToString();
         }
 
-        private void Result_click(object sender, RoutedEventArgs e)
+        private void Result_Click(object sender, RoutedEventArgs e)
         {
             try
             {
                 string @operator = "";
-                int firstNumberLength = 0;
 
-                if (resultWindow.Text.Contains("+")) 
-                {
-                    firstNumberLength = resultWindow.Text.IndexOf("+");
-                }
-                else if (resultWindow.Text.Contains("-"))
-                {
-                    firstNumberLength = resultWindow.Text.IndexOf("-");
-                }
-                else if (resultWindow.Text.Contains("*"))
-                {
-                    firstNumberLength = resultWindow.Text.IndexOf("*");
-                }
-                else if (resultWindow.Text.Contains("/"))
-                {
-                    firstNumberLength = resultWindow.Text.IndexOf("/");
-                }
+                int firstNumberLength = GetFirstNumberLength();
 
-                @operator = resultWindow.Text.Substring(firstNumberLength, 1); 
+                @operator = ResultWindow.Text.Substring(firstNumberLength, 1);
 
-                double firstNumber = double.Parse(resultWindow.Text.Substring(0, firstNumberLength));
-                double secondNumber = double.Parse(resultWindow.Text.Substring(firstNumberLength + 1, resultWindow.Text.Length - firstNumberLength - 1));
+                double firstNumber = double.Parse(ResultWindow.Text.Substring(0, firstNumberLength));
+                double secondNumber = double.Parse(ResultWindow.Text.Substring(firstNumberLength + 1, ResultWindow.Text.Length - firstNumberLength - 1));
 
-                if (@operator == "+")
-                {
-                    resultWindow.Text += "=" + (firstNumber + secondNumber);
-                }
-                else if (@operator == "-")
-                {
-                    resultWindow.Text += "=" + (firstNumber - secondNumber);
-                }
-                else if (@operator == "*")
-                {
-                    resultWindow.Text += "=" + (firstNumber * secondNumber);
-                }
-                else
-                {
-                    resultWindow.Text += "=" + (firstNumber / secondNumber);
-                }
+                GetResult(@operator, firstNumber, secondNumber);
+
+                newNumbers = true;
             }
             catch
             {
-                resultWindow.Text = "INVALID INPUT!";
+                ResultWindow.Text = "INVALID INPUT!";
+            }
+        }
+
+        private int GetFirstNumberLength()
+        {
+            int firstNumberLength = 0;
+
+            if (ResultWindow.Text.Contains("+"))
+            {
+                firstNumberLength = ResultWindow.Text.IndexOf("+");
+            }
+            else if (ResultWindow.Text.Contains("*"))
+            {
+                firstNumberLength = ResultWindow.Text.IndexOf("*");
+            }
+            else if (ResultWindow.Text.Contains("/"))
+            {
+                firstNumberLength = ResultWindow.Text.IndexOf("/");
+            }
+            else if (ResultWindow.Text.Contains("%"))
+            {
+                firstNumberLength = ResultWindow.Text.IndexOf("%");
+            }
+            else if (ResultWindow.Text.Contains("-"))
+            {
+                firstNumberLength = ResultWindow.Text[ResultWindow.Text.LastIndexOf("-") - 1].ToString() == "-" ? ResultWindow.Text.LastIndexOf("-") - 1 : ResultWindow.Text.LastIndexOf("-");
+            }
+
+            return firstNumberLength;
+        }
+
+        private void GetResult(string @operator, double firstNumber, double secondNumber)
+        {
+            switch (@operator)
+            {
+                case "+": ResultWindow.Text = (firstNumber + secondNumber).ToString(); break;
+                case "-": ResultWindow.Text = (firstNumber - secondNumber).ToString(); break;
+                case "*": ResultWindow.Text = (firstNumber * secondNumber).ToString(); break;
+                case "/": ResultWindow.Text = (firstNumber / secondNumber).ToString(); break;
+                case "%": ResultWindow.Text = ((firstNumber * secondNumber) / 100).ToString(); break;
             }
         }
 
         private void C_Click(object sender, RoutedEventArgs e)
         {
-            resultWindow.Text = "";
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            this.Close();
+            ResultWindow.Text = "";
         }
     }
 }
